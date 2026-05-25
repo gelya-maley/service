3136925c980fdb74880962905393e45f58895d95
gelya-maley <maley.gelya@mail.ru>
2026-05-25 06:07:13 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
-    # ВРЕМЕННО для создания суперпользователя
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
